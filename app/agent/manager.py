647099faec1b945eb8744e6678898573304fc49a
floris <florisdroppert@gmail.com>
2023-04-18 20:30:10 +0200

    updating comments

--- app/agent/manager.py
+++ app/agent/manager.py
@@ -4,14 +4,17 @@ from schemas import MessageDict
 
 
 def start_agent(name: str, task: str, prompt: str, model="gpt-3.5-turbo") -> str:
-    """Start an agent with a given name, task, and prompt
+    """
+    Start an agent with a given name, task, and prompt.
+
     Args:
-        name (str): The name of the agent
-        task (str): The task of the agent
-        prompt (str): The prompt for the agent
-        model (str): The model to use for the agent
+        name (str): The name of the agent.
+        task (str): The task of the agent.
+        prompt (str): The prompt for the agent.
+        model (str, optional): The AI model to use for the agent. Default is "gpt-3.5-turbo".
+
     Returns:
-        str: The response of the agent
+        str: The response of the agent.
     """
 
     first_message = f"""You are {name}.  Respond with: "Acknowledged"."""
@@ -57,7 +60,15 @@ def delete_agent(key: str) -> str:
 
 
 class AgentManager:
-    def __init__(self):
+    """
+    AgentManager class manages agents with different tasks and prompts.
+
+    Attributes:
+        next_key (int): The next key to be assigned to a new agent.
+        agents (dict): Dictionary containing agents with their key, task, full message history, and model.
+    """
+
+    def __init__(self) -> None:
         self.next_key = 0
         self.agents: dict[
             int, tuple[str, list[MessageDict], str]
@@ -66,6 +77,17 @@ class AgentManager:
     def create_agent(
         self, task: str, prompt: str, model="gpt-3.5-turbo"
     ) -> tuple[int, str]:
+        """
+        Create a new agent with the given task, prompt, and model.
+
+        Args:
+            task (str): The task of the agent.
+            prompt (str): The prompt for the agent.
+            model (str, optional): The AI model to use for the agent. Default is "gpt-3.5-turbo".
+
+        Returns:
+            tuple[int, str]: A tuple containing the key of the new agent and its response.
+        """
         conversation: list[MessageDict] = [
             {"role": "user", "content": prompt},
         ]
@@ -80,7 +102,16 @@ class AgentManager:
         return key, conversation[-1]["content"]
 
     def message_agent(self, key: str | int, message: str) -> str:
-        """Send a message to an agent and return its response"""
+        """
+        Send a message to an agent with the specified key and return its response.
+
+        Args:
+            key (str): The key of the agent to send the message to.
+            message (str): The message to send to the agent.
+
+        Returns:
+            str: The response of the agent.
+        """
         task, conversation, model = self.agents[int(key)]
 
         # Add user message to message history before sending to agent
@@ -95,20 +126,25 @@ class AgentManager:
         return conversation[-1]["content"]
 
     def list_agents(self) -> list[tuple[str | int, str]]:
-        """Return a list of all agents
+        """
+        List all agents managed by the AgentManager.
+
         Returns:
-            A list of tuples of the form (key, task)
+            str: A formatted string containing the list of all agents with their keys and tasks.
         """
 
         # Return a list of agent keys and their tasks
         return [(key, task) for key, (task, _, _) in self.agents.items()]
 
     def delete_agent(self, key: str | int) -> bool:
-        """Delete an agent from the agent manager
+        """
+        Delete an agent with the given key from the AgentManager.
+
         Args:
-            key: The key of the agent to delete
+            key (str): The key of the agent to delete.
+
         Returns:
-            True if successful, False otherwise
+            str: A message indicating whether the agent was deleted or not.
         """
 
         try:
