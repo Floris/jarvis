bc881d8971c3d225f2f39192255e2642d2e430f3
floris <florisdroppert@gmail.com>
2023-04-22 15:38:41 +0200

    updated tests

--- tests/ai/test_manager.py
+++ tests/ai/test_manager.py
@@ -1,55 +1,80 @@
+from unittest.mock import patch
+
 import pytest
 
 from app.ai.manager import AgentManager
-from tests.mocks.chat import mock_generate_chat
+
+mocked_openai_create_chatcompletion = {
+    "id": "chatcmpl-123",
+    "object": "chat.completion",
+    "created": 1677652288,
+    "model": "gpt-3.5-turbo",
+    "choices": [
+        {
+            "index": 0,
+            "message": {
+                "role": "assistant",
+                "content": "\n\nHello there, how may I assist you today?",
+            },
+            "finish_reason": "stop",
+        }
+    ],
+    "usage": {"prompt_tokens": 9, "completion_tokens": 12, "total_tokens": 21},
+}
 
 
-@mock_generate_chat()
 @pytest.fixture
-def agent_manager():
+def agent_manager() -> AgentManager:
     agent_manager = AgentManager()
 
     # Create an agent
     task = "Sample Task"
     prompt = "Sample Prompt"
-    key, _ = agent_manager.create_agent(task, prompt)
+
+    with patch("openai.ChatCompletion.create") as mock_function:
+        mock_function.return_value = mocked_openai_create_chatcompletion
+        key, _ = agent_manager.create_agent(task, prompt)
 
     return agent_manager
 
 
-@mock_generate_chat()
-def test_create_agent(agent_manager):
+def test_create_agent(agent_manager: AgentManager) -> None:
     task = "Test task"
     prompt = "Test prompt"
     model = "gpt-3.5-turbo"
 
-    key, response = agent_manager.create_agent(task, prompt, model)
+    with patch("openai.ChatCompletion.create") as mock_function:
+        mock_function.return_value = mocked_openai_create_chatcompletion
+        key, response = agent_manager.create_agent(task, prompt, model)
+
     assert key == 1
     assert isinstance(response, str)
     assert agent_manager.agents[key][0] == task
     assert agent_manager.agents[key][2] == model
 
 
-@mock_generate_chat()
-def test_message_agent(agent_manager):
+def test_message_agent(agent_manager: AgentManager) -> None:
     key = 0
 
-    response = agent_manager.message_agent(key, "Test message")
+    with patch("openai.ChatCompletion.create") as mock_function:
+        mock_function.return_value = mocked_openai_create_chatcompletion
+        response = agent_manager.message_agent(key, "Test message")
+
     assert isinstance(response, str)
     assert (
         agent_manager.agents[key][1][-1]["content"]
-        == "\n\nHello there, this is a mocked response?"
+        == "\n\nHello there, how may I assist you today?"
     )
 
 
-def test_list_agents(agent_manager):
+def test_list_agents(agent_manager: AgentManager) -> None:
     agents = agent_manager.list_agents()
     assert len(agents) == 1
     assert agents[0][0] == 0
     assert agents[0][1] == "Sample Task"
 
 
-def test_delete_agent(agent_manager):
+def test_delete_agent(agent_manager: AgentManager) -> None:
     key = 0
     assert agent_manager.delete_agent(key) is True
     assert agent_manager.agents.get(key) is None
